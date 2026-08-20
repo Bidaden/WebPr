cdf72455ad7d31a45c7ecc6d13afb8d5b950aa8b
Bidaden <phanvu202201@gmail.com>
2026-08-20 22:17:39 +0700

    Refactor User class and add UserDB for storage

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,45 +1,24 @@
 package com.example;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class User implements Serializable {
-    private String firstName;
-    private String lastName;
-    private String email;
-
-    public User() {
-        firstName = "";
-        lastName = "";
-        email = "";
+public class UserDB {
+    
+    private static final List<User> users = new ArrayList<>();
+    
+    public static void save(User user) {
+        users.add(user);
+        
+        System.out.println("=== USER SAVED ===");
+        System.out.println("First Name: " + user.getFirstName());
+        System.out.println("Last Name: " + user.getLastName());
+        System.out.println("Email: " + user.getEmail());
+        System.out.println("Total users in memory: " + users.size());
+        System.out.println("==================");
     }
-
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    
+    public static List<User> getAllUsers() {
+        return new ArrayList<>(users);
     }
 }
